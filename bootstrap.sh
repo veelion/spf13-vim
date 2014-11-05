b87d7f2fbd10a2238b5b565878bb013549c629f3
veelion <veelion@gmail.com>
2014-11-05 14:28:29 +0800

    fix endpath

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,8 @@
 ############################  SETUP PARAMETERS
 app_name='spf13-vim'
 app_dir="$HOME/.spf13-vim-3"
-[ -z "$git_uri" ] && git_uri='https://github.com/spf13/spf13-vim.git'
+endpath="$HOME/.spf13-vim-3"
+[ -z "$git_uri" ] && git_uri='https://github.com/veelion/spf13-vim.git'
 git_branch='3.0'
 debug_mode='0'
 fork_maintainer='0'
